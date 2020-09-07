ada6bb938b31c3efb0d300c58b563e8323f839e3
devevangel <dev.evangel@gmail.com>
2020-09-08 00:28:12 +0100

    Finishing touches for deployment

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const mongoose = require('mongoose');
 
 const app = require('./app');
 
-const DB =
-  process.env.DATABASE_LOCAL ||
-  process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD);
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
 
 mongoose
   .connect(DB, {
